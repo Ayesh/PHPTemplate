f6390170675f9ddd79cd2aba1761a147cb0a9768
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-10-27 00:07:24 +0400

    Add fixture for the benchmark

--- tests/fixtures/template-benchmark.php
+++ tests/fixtures/template-benchmark.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <title><?= $v['foo']; ?></title>
+</head>
+<body>
+
+<h1><?= $v['bar']; ?></h1>
+<p><?= $v['foo']; ?></p>
+<a href="<?= $v[':url_safe']; ?>">Test</a>
+<a href="<?= $v[':url_unsafe']; ?>">Test</a>
+
+<?= $v['non-existent']; ?>
+<?= $v['!non-existent']; ?>
+<?= $v['@non-existent']; ?>
+
+<span <?= $v->attributes($v->get('attr')); ?>></span>
+
+</body>
+</html>
